74178305bdbf1917432781ca929fd87ceda8ed94
vrushali09 <vgaikwad@stevens.edu>
2016-04-17 22:03:13 -0400

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include "mainwindow.h"
-#include "logino.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow* mainWindow = new MainWindow();
-    Logino* logino = new Logino(mainWindow);
-    logino->show();
-    mainWindow->show();
+    MainWindow w;
+    w.show();
 
     return a.exec();
 }
