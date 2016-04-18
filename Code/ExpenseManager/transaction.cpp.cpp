39225bebdb209671e32dadbbb6c692d8d76ec308
vrushali09 <vgaikwad@stevens.edu>
2016-04-18 00:39:58 -0400

    Added files via upload

---
+++ Code/ExpenseManager/transaction.cpp.cpp
@@ -0,0 +1,55 @@
+/*\file transaction.cpp
+ *
+ * \brief   Transaction source file containing required methods
+ *
+ * Revision history:
+ *
+ * Date        Author                Changes
+ * 04/18/2016  Vrushali Gaikwad      Created file and framework
+ *
+ *
+ */
+#include "transaction.h"
+
+// constructor
+transaction::transaction()
+{
+
+}
+
+// Method to show th transaction details given by user
+transaction::showtransactionpage()
+{
+
+}
+
+transaction::showAmounttextBox()
+{
+
+}
+
+transaction::showDescriptiontextBox()
+{
+
+}
+
+transaction::showCCategorytextBox()
+{
+
+}
+
+transaction::showpaymenttypebox()
+{
+
+}
+
+transsaction::showDatefield()
+{
+
+}
+
+transaction::submitbutton()
+{
+
+}
+
