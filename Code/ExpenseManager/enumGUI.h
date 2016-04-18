0d4782299e808fe18b51eda468cae3f276aa9f50
vrushali09 <vgaikwad@stevens.edu>
2016-04-18 13:48:09 -0400

    Added files via upload

---
+++ Code/ExpenseManager/enumGUI.h
@@ -0,0 +1,26 @@
+/*\file enum.h
+ *
+ * \brief     Header file containing different enum definitions
+ *
+ * Revision History:
+ *
+ * Date            Author                Changes
+ * 04/18/2016      Vrushali Gaikwad      Created file and adde enum definitions
+ *
+ */
+#ifndef ENUMH_H
+#define ENUMH_H
+
+
+// Type of Graph
+
+enum graphType_t {
+        DAILY = 0,
+        WEEKLY,
+        MONTHLY,
+        YEARLY,
+        MAX_GRAPH_TYPE
+};
+
+
+#endif // ENUMH_H
