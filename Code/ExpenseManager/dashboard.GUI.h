0bd99fa886d8f85b32d13803a8cfbffdca565ad8
vrushali09 <vgaikwad@stevens.edu>
2016-04-18 11:50:24 -0400

    Added files via upload

---
+++ Code/ExpenseManager/dashboard.GUI.h
@@ -0,0 +1,30 @@
+/*\ file dashboard.h
+ *
+ * \brief   dashboard header file containing class definition
+ *
+ * revision history
+ * Date           Author                 Changes
+ * 04/18/2016     Vrushali Gaikwad       Created file and added members and methods
+ *
+ *
+ */
+#ifndef DASHBOARD_H
+#define DASHBOARD_H
+
+
+class dashboard
+{
+public:
+    // constructor
+    dashboard();
+    // show methods
+    showusername();
+    showpassword();
+    showfigure();
+    showtransaction();
+    showoptiontab();
+    showincomeButton();
+    showexpensebutton();
+};
+
+#endif // DASHBOARD_H
