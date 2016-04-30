90a93a2ff0fe266b45572a8d6cdd49e98c5ec1cf
vrushali09 <vgaikwad@stevens.edu>
2016-04-30 14:47:44 -0400

    Added files via upload

---
+++ Code/ExpenseManager/logino.h
@@ -0,0 +1,36 @@
+#ifndef LOGINO_H
+#define LOGINO_H
+
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QMessageBox>
+
+//Author - Vrushali Gaikwad
+
+class Logino : public QDialog
+{
+    Q_OBJECT
+
+public:
+    Logino(QWidget* parent = 0);
+    ~Logino();
+
+private slots:
+    void OnQuit();
+    void OnLogin();
+
+private:
+    void reject();
+
+    QLabel* userLabel;
+    QLabel* passLabel;
+    QLineEdit* userLineEdit;
+    QLineEdit* passLineEdit;
+    QPushButton* loginButton;
+    QPushButton* quitButton;
+};
+#endif // LOGINO_H
---
+++ Code/ExpenseManager/main.cpp
@@ -0,0 +1,16 @@
+//Author- Vrushali Gaikwad
+
+#include "mainwindow.h"
+#include "logino.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow* mainWindow = new MainWindow();
+    Logino* logino = new Logino(mainWindow);
+    logino->show();
+    mainWindow->show();
+
+    return a.exec();
+}
